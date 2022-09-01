61b2c6d8d57415f65445058bd3d04c824e6b0145
arjundevlucid <Arjun.Sahu@Lucidplus.com>
2022-09-01 17:49:14 +0530

    changed dbservice interface

--- lib/shared/riverpod/db_service_provider.dart
+++ lib/shared/riverpod/db_service_provider.dart
@@ -1,8 +1,9 @@
 import 'package:blog_app_riverpod/data/service/db/db_service.dart';
+import 'package:blog_app_riverpod/data/service/db/i_db_service.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final dbServiceProvider = Provider.autoDispose(
+final dbServiceProvider = Provider.autoDispose<IDbService>(
   (ref) {
     return DbService();
   },
